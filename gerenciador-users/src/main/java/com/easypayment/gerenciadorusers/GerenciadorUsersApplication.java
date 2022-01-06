ed87963967f38d71b8b629865e348bfa09c8542b
Vinicius Santos <viniciushacrinha1@gmail.com>
2022-01-06 14:05:51 -0300

    init

---
+++ gerenciador-users/src/main/java/com/easypayment/gerenciadorusers/GerenciadorUsersApplication.java
@@ -0,0 +1,13 @@
+package com.easypayment.gerenciadorusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorUsersApplication.class, args);
+	}
+
+}
---
+++ gerenciador-users/src/test/java/com/easypayment/gerenciadorusers/GerenciadorUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypayment.gerenciadorusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
